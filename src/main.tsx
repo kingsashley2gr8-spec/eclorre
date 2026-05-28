5fcfa34e9656c001866dc3c8a7576097204ba1f9
kingsashley2gr8-spec <kingsashley2gr8@gmail.com>
2026-05-28 13:49:00 -0500

    Initialize main.tsx with ReactDOM rendering

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EcloreLanding from './app'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <EcloreLanding />
+  </React.StrictMode>
+)
